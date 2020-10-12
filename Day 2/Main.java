95f9ca797995c718a3f874b499eef7d3e7cdf8b0
Akash Arjun Sawant <66379217+sky106@users.noreply.github.com>
2020-10-13 02:17:00 +0530

    Add files via upload

---
+++ Day 2/Main.java
@@ -0,0 +1,20 @@
+package com.day2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee();
+        e1.name = "Akash";
+        e1.age = 23;
+        e1.city = "Mumbai";
+        e1.display();
+
+        Employee e2 = new Employee();
+        e2.name = "Pramod";
+        e2.age = 24;
+        e2.city = "Pune";
+        e2.display();
+
+    }
+}
