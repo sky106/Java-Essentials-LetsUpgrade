ab04bf898ce24860ebcf31b2e6ec34bdb93a05e9
Akash Arjun Sawant <66379217+sky106@users.noreply.github.com>
2020-10-15 13:40:01 +0530

    Add files via upload

---
+++ Day 3/Question-2 (solution)/Main.java
@@ -0,0 +1,14 @@
+//Day 3 Question 2
+
+package com.day3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee em = new Employee();
+        em.getDetails();
+        em.displayDetails();
+
+    }
+}
