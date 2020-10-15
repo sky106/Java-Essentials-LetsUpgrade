a3449a34a70b64a470d321a2deb1c4e6ac8b3f7e
Akash Arjun Sawant <66379217+sky106@users.noreply.github.com>
2020-10-15 13:37:07 +0530

    Add files via upload

---
+++ Day 3/Question-1 (solution)/Main.java
@@ -0,0 +1,14 @@
+//Day 3 Question 1
+
+package com.day3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Percentage pc = new Percentage();
+        pc.getMarks();
+        pc.displayDetails();
+
+    }
+}
