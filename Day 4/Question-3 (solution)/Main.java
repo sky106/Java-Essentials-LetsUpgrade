85e21a66d5846f8c226d9e91b158c3767a5d6306
Akash Arjun Sawant <66379217+sky106@users.noreply.github.com>
2020-10-15 15:24:51 +0530

    Add files via upload

---
+++ Day 4/Question-3 (solution)/Main.java
@@ -0,0 +1,14 @@
+//Day 4 Question 3
+
+package com.day4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Sum sm = new Sum();
+        sm.getNumbers();;
+        sm.displaySum();
+
+    }
+}
