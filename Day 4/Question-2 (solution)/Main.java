808bdb3817358dcbdab9df63112c68596fc082ef
Akash Arjun Sawant <66379217+sky106@users.noreply.github.com>
2020-10-15 15:22:50 +0530

    Add files via upload

---
+++ Day 4/Question-2 (solution)/Main.java
@@ -0,0 +1,14 @@
+//Day 4 Question 2
+
+package com.day4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Numbers nm = new Numbers();
+        nm.getNumbers();
+        nm.displayOddNumbers();
+
+    }
+}
